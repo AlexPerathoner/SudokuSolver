6c0c559d42e4bcd94f87c00e45cd55a5cee9dd24
Alex <alex.perathoner@gmail.com>
2020-01-02 18:38:14 +0100

    Initial Commit

---
+++ SudokuSolver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SudokuSolver
+//
+//  Created by Alex Perathoner on 02/01/2020.
+//  Copyright © 2020 Alex Perathoner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

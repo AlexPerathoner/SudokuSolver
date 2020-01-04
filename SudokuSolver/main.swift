44ce3bb5d4773344e1187690b8233affaf94a534
Alex <alex.perathoner@gmail.com>
2020-01-04 15:11:10 +0100

    Adding default path; Adding (and commenting) different combinations calculator

--- SudokuSolver/main.swift
+++ SudokuSolver/main.swift
@@ -13,14 +13,29 @@ import Foundation
 var matrix: Matrix<Int?> = Matrix(rows: 9, columns: 9,defaultValue:nil)
 
 print("Insert path of file with sudoku to solve: ")
-let path = readLine()
+var path = readLine()!
+let defaultPath = "/Users/alex/Desktop/sudoku3.txt"
+if(path == "") {path = defaultPath}
 
 
-matrix = stringToTable(getFileContent(path: path ?? "/Users/alex/Desktop/sudoku3.txt")!)
+matrix = stringToTable(getFileContent(path: path)!)
 
 print(tableToString(matrix))
 print("Solving...")
 var historyMoves = [(x: Int, y: Int, value: Int)]()
+
+/*
+var combinations: CUnsignedLongLong = 1
+for i in getPossibleElements(matrix).grid {
+	if (i.count > 0)
+	{
+		combinations = combinations * UInt64(i.count)
+		
+	}
+}
+print("There are \(combinations) combinations in this sudoku!")
+*/
+
 let methodStart = Date()
 if(solve(m: &matrix, old: &historyMoves)) {
 	print("Solved!")
